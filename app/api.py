cb1d7d1963bac2ee0ae47d6ea13471cc41b22ff5
BuStarley <dmitriyrepnin@yandex,com>
2025-09-16 09:40:44 +0300

    Fix user-response

--- app/api.py
+++ app/api.py
@@ -3,7 +3,7 @@ from models import User
 
 router = APIRouter()
 
-users = [User(name="<NAME>", email="<EMAIL>", password="<PASSWORD>")]
+users = [User(name="<NAME>", email="<EMAIL>", password="<PASSWORD>", id=0)]
 
 @router.get("/users")
 async def get_users():
@@ -13,5 +13,6 @@ async def get_users():
 async def register_user(user: User):
     if any(u.email == user.email for u in users):
         raise HTTPException(status_code=400, detail="Email already registered")
+    user.id = len(users)
     users.append(user)
-    return user
+    return {"message": "User registered successfully"}
