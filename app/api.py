92cfe97e30856cdb85f175c8cf666bb8576e7a54
BuStarley <dmitriyrepnin@yandex,com>
2025-09-16 09:35:27 +0300

    Added module register in api.py

--- app/api.py
+++ app/api.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from models import User
 
 router = APIRouter()
@@ -8,3 +8,10 @@ users = [User(name="<NAME>", email="<EMAIL>", password="<PASSWORD>")]
 @router.get("/users")
 async def get_users():
     return users
+
+@router.post("/register")
+async def register_user(user: User):
+    if any(u.email == user.email for u in users):
+        raise HTTPException(status_code=400, detail="Email already registered")
+    users.append(user)
+    return user
