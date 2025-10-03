8c22a1b60abaf259e98a6d5a14018c7d1e1b7315
BuStarley <dmitriyrepnin@yandex,com>
2025-10-03 21:48:56 +0300

    Create jwt-token module

---
+++ app/token.py
@@ -0,0 +1,29 @@
+
+from datetime import datetime, timedelta
+from fastapi import HTTPException
+from jose import jwt, JWTError
+
+
+SECRET_KEY = "your-secret-key-change-this"
+ALGORITHM = "HS256"
+
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now() + expires_delta
+    else:
+        expire = datetime.now() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise HTTPException(status_code=401, detail="Invalid authentication credentials")
+        return email
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid authentication credentials")
