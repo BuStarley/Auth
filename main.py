965ab71368047607f9af7fd657f00883ee6dd2af
BuStarley <dmitriyrepnin@yandex,com>
2025-10-18 14:09:38 +0300

    Create docker containers

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from fastapi import FastAPI
 from app.database import engine, Base
 from app.api import router
 
-__name__ = "main"
-
 Base.metadata.create_all(bind=engine)
+
 app = FastAPI(tytle="User API")
 app.include_router(router)
 
